fdce93318b6dce386eb4a35efe48847d52029409
ABDELRAHIM SAEED <abdulrhimsaeed@gmail.com>
2023-11-30 15:23:46 -0800

    python

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    def add(x, y):
+        return x + y
+
+    def sub(x, y):
+        return x - y
+
+    if a < b:
+        c = add(a, b)
+
+        for i in range(4, 6):
+            c = add(c, i)
+
+        return c
+    else:
+        return sub(a, b)
