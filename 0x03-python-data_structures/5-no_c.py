7a4cd1f2ee5624a478f2b475cd96e3fbb8a8dd87
ABDELRAHIM SAEED <abdulrhimsaeed@gmail.com>
2023-12-04 14:52:05 -0800

    python

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,8 @@ def no_c(my_string):
     if my_string == None:
         return None
     else:
-        new_string = my_string.replace('c', '').replace('C', '')
-        return new_string
+        result = ''
+        for char in my_string:
+            if char != 'c' and char != 'C':
+                result += char
+        return result
