cb193dd942e840e623ba56480a02e0fcea6aa6c2
ABDELRAHIM SAEED <abdulrhimsaeed@gmail.com>
2023-12-19 14:58:34 -0800

    python

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+""" Module 3-square: class Square """
 class Square:
+"""
+        Square: defines a square.
+        Attributes:
+            size (int): size of square.
+        Method:
+                __init__ : init of size attribute for each instance.
+    """
     def __init__(self, size=0):
+        """ Initialization of attributes for instances
+            Args:
+                size (int): size of the square.
+        """
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+""" Module 4-square: class Square """
 class Square:
+    """
+        Square: defines a square.
+        Attributes:
+            size (int): size of square.
+        Method:
+                __init__ : init of size attribute for each instance.
+    """
     def __init__(self, size=0):
+        """ Initialization of attributes for instances
+            Args:
+                size (int): size of the square.
+        """
         self.__size = size
 
     def size(self):
