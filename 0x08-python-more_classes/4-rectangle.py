24d66b11400a09227d77fa6c2d2f11f5e7cd44ff
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-01-31 14:26:29 -0800

    python

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -46,3 +46,5 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ""
         return "\n".join(["#" * self.width] * self.height)
+    def __repr__(self):
+        return "Rectangle({},{})".format(self.width,self.height)
