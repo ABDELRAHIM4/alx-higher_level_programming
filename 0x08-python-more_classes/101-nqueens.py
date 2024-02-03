81e81ab8b1e8566a0f7b8792c088191a541c8490
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-02-03 14:14:37 -0800

    python

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,49 +1,54 @@
-#!/usr/bin/python3
-
 import sys
 
-if len(sys.argv) != 2:
-    print('Usage: nqueens N')
-    print('N must be an integer greater or equal to 4')
-    sys.exit(1)
+def nqueens(n):
+    if not isinstance(n, int) or n < 4:
+        print("N must be a number")
+        print("N must be at least 4")
+        sys.exit(1)
 
-try:
+    def is_safe(board, row, col):
+        for i in range(row):
+            if board[i][col] == 1:
+                return False
+
+        for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+            if board[i][j] == 1:
+                return False
+
+        for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+            if board[i][j] == 1:
+                return False
+
+        return True
+
+    def solve(board, col):
+        if col >= n:
+            print(board)
+            return
+
+        for i in range(n):
+            if is_safe(board, i, col):
+                board[i][col] = 1
+
+                solve(board, col + 1)
+
+                board[i][col] = 0
+
+    board = [[0 for _ in range(n)] for _ in range(n)]
+
+    solve(board, 0)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
     n = int(sys.argv[1])
-except ValueError:
-    print('N must be a number')
-    sys.exit(1)
 
-if n < 4:
-    print('N must be at least 4')
-    sys.exit(1)
+    if not isinstance(n, int) or n < 4:
+        print("N must be a number")
+        print("N must be at least 4")
+        sys.exit(1)
 
-board = [[-1 for _ in range(n)] for _ in range(n)]
-
-
-def is_attacking(x, y):
-    """Check if a queen at (x, y) is attacking another queen."""
-    for i in range(n):
-        if board[i][y] == 1 or board[x][i] == 1:
-            return True
-    for i, j in zip(range(x, -1, -1), range(y, -1, -1)):
-        if board[i][j] == 1:
-            return True
-    for i, j in zip(range(x, n), range(y, -1, -1)):
-        if board[i][j] == 1:
-            return True
-    return False
-
-
-def solve(row):
-    """Solve the N queens problem using backtracking."""
-    for col in range(n):
-        if not is_attacking(row, col):
-            board[row][col] = 1
-            if row < n - 1:
-                solve(row + 1)
-                board[row][col] = 0
-            else:
-                print(board)
-
-
-solve(0)
+    nqueens(n)
