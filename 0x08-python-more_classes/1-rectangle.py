52675e4558e89a845c7d6c453f622758f34f27f7
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-02-03 05:32:21 -0800

    python

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,32 +5,29 @@ This module defines a rectangle object
 class Rectangle:
     """ definition a class rectangle """
     def __init__(self, width=0, height=0):
-         """initializing a rectangle """
-        self._width = width
-        self._height = height
-
+        """initializing a rectangle """
+        self.width = width
+        self.height = height
     @property
     def width(self):
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
-        """ set the width of rectangle """
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
-        if value < 0:
+        elif value < 0:
             raise ValueError('width must be >= 0')
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """ set the height of rectangle """
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
-        if value < 0:
+        elif value < 0:
             raise ValueError('height must be >= 0')
-        self._height = value
+        self.__height = value
