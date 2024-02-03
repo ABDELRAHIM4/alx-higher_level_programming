32cc991d5dec150dd21069ddf7418ea237165d71
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-02-03 12:48:02 -0800

    python

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -10,11 +10,9 @@ class Rectangle:
         self.__width = width
         self.__height = height
         Rectangle.number_of_instances += 1
-
     @property
     def width(self):
         return self.__width
-
     @width.setter
     def width(self, value):
         """ set the width of rectangle """
@@ -23,11 +21,9 @@ class Rectangle:
         if value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
-
     @property
     def height(self):
         return self.__height
-
     @height.setter
     def height(self, value):
         """ set the height of rectangle """
@@ -36,30 +32,24 @@ class Rectangle:
         if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
-
     def area(self):
         """ calculate the area of rectangle """
         if self.__width == 0 or self.__height == 0:
             return 0
-
         return self.__width * self.__height
-
     def perimeter(self):
         """calculate the parimeter of rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
-
     def __str__(self):
         """ print the rectangle """
         if self.__width == 0 or self.__height == 0:
             return ''
         return '\n'.join(['#' *self.__width] * self.__height)
-
     def __repr__(self):
         """ print the rectangle usung eval"""
         return 'Rectangle({}, {})'.format(self.__width, self.__height)
-
     def __del__(self):
         """ print the message when the rectangle is deleted  """
         print('Bye rectangle...')
