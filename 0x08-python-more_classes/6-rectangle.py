349f4c16347d1e89312047df71db08ee68e8939d
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-02-03 06:29:49 -0800

    python

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -39,25 +39,26 @@ class Rectangle:
 
     def area(self):
         """ calculate the area of rectangle """
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return self.width * self.height
+
+        return self.__width * self.__height
 
     def perimeter(self):
         """calculate the parimeter of rectangle"""
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.width + self.height)
+        return 2 * (self.__width + self.__height)
 
     def __str__(self):
         """ print the rectangle """
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return ''
-        return '\n'.join([self.print_symbol *self.width] * self.height)
+        return '\n'.join([self.print_symbol *self.__width] * self.__height)
 
     def __repr__(self):
         """ print the rectangle usung eval"""
-        return 'Rectangle({}, {})'.format(self.width, self.height)
+        return 'Rectangle({}, {})'.format(self.__width, self.__height)
 
     def __del__(self):
         """ print the message when the rectangle is deleted  """
