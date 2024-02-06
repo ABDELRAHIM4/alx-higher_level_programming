2a8358373d15471c40b72f6623ecd7918914101e
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-02-06 13:09:39 -0800

    python

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+"""function that reads a text file (UTF8) and prints it to stdout"""
 def read_file(filename=""):
-    """function that writes a string to a text file """
+    """function that reads a text file (UTF8) and prints it to stdout"""
     with open(filename,"r",encoding = 'UTF8') as file:
         file.read()
