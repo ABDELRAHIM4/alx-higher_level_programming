4c6b3ada3a0785da6168b2b43080fd2add3e1faf
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-02-05 17:02:59 -0800

    python

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+""" create function that returns True if the object is an instance of a class that inherited """
 def inherits_from(obj, a_class):
-    """ Returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False """
+    """ Returns True if the object is an instance of a class that inherited (directly or indirectly) """
     if isinstance(obj, a_class) and type(obj) != a_class:
         return True
     else:
