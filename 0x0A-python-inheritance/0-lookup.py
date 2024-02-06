cafb3c3d34717d5b8d6bc7404eb31ccf52b81768
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-02-05 17:37:17 -0800

    python

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Create function """
+""" Creatinrg a function """
 def lookup(obj):
     """ Returns the list of available attributes and methods of an object """
-     return dir(obj)
+    return dir(obj)
