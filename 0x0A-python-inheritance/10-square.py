7c11996aa79b8acf516ee2febacca501471e8c13
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-02-07 14:39:08 -0800

    python

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -27,10 +27,11 @@ class Rectangle(BaseGeometry):
         """ __str__ method """
         return "[Rectangle]{}/{}" .format(self.__size,self.__size)
 class Square(Rectangle):
+    """ Class Square that inherits from Rectangle"""
     def __init__(self, size):
+        """Instantiation with size"""
         self.integer_validator("size",size)
         self.__size = size
     def area(self):
+        """area() method """
         return self.__size* self.__size
-    def __str__(self):
-        return "[Square] {}/{}" .format(self.__size,self.__size)
