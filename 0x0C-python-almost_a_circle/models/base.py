2d56d1c9b0d0e83dc139c2088fbdbbf780cafd4e
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-02-12 12:07:23 -0800

    pyhton

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ first class Base"""
 class Base:
-    """ first class Base"""
     """private class attribute __nb_objects = 0"""
     __nb_objects = 0
     def __init__(self, id=None):
