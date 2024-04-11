c56db4f233c39b5d4e61fb002a8eb67aceccabee
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-04-10 17:00:25 -0700

    java

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./4-rectangle.js'){
 	constructor(size){
-		super(size);
+		super(size, size);
 	}
 
-charPrint(c){
-	if (c == undefined){
-		c = 'X';
+	charPrint(c){
+		if (c == undefined){
+			c = 'X';
+		}
+		for (let i = 0; i < this.height; i++)
+		{
+			console.log(c.repeat(this.width));
+		}
 	}
-	for (let i = 0; i <this.height; i++)
-	{
-		console.log(c.repeat(this.width));
-	}
-}
 }
