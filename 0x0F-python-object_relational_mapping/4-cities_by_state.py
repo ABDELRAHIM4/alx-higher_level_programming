6efab4d69dea73bb11663bc58e11ea62ec2c8624
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-04-18 12:13:35 -0700

    sql

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,6 +4,7 @@ import sys
 import MySQLdb
 """script that lists all cities from the database hbtn_0e_4_usa"""
 if __name__ == "__main__":
+    """script that lists all cities from the database hbtn_0e_4_usa"""
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
