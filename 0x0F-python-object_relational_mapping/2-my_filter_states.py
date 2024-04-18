b9cf5e085e4a23442db14bed7cd9a99c7c94f772
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-04-18 08:47:38 -0700

    sql

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    select_n = sys.argv[4]
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    cursor = db.cursor()
+    cursor.execute("SELECT DISTINCT * FROM states WHERE name LIKE '{}%' ORDER BY id ASC".format(select_n))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
