c402fcdba94a3248478d62fdccbdfc5c89cfc100
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-04-26 06:59:12 -0700

    network

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-#Python script that takes in a URL
+"""Python script that takes in a URL"""
 import urllib.request
 import sys
 if __name__ == "__main__":
     val = sys.argv[1]
     with urllib.request.urlopen(val) as response:
-        res = response.getheader('X-Request-Id')
+        res = response.getheader("X-Request-Id")
         print(res)
