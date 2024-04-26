befb891bcaabc07b0229ac1d7eb978a3ec5fd221
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-04-26 06:55:49 -0700

    network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Python script that fetches"""
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    content = response.read()
-    print("Body response:")
-    print("- type: {}".format(type(content)))
-    print("- content: {}".format(content))
-    print("- utf8 content: {}".format(content.decode('utf-8')))
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("- type: {}".format(type(content)))
+        print("- content: {}".format(content))
+        print("- utf8 content: {}".format(content.decode('utf-8')))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,7 +2,8 @@
 #Python script that takes in a URL
 import urllib.request
 import sys
-val = sys.argv[1]
-with urllib.request.urlopen(val) as response:
-    res = response.getheader('X-Request-Id')
-    print(res)
+if __name__ == "__main__":
+    val = sys.argv[1]
+    with urllib.request.urlopen(val) as response:
+        res = response.getheader('X-Request-Id')
+        print(res)
