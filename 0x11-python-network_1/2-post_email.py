642c7b7ca351de84b9ec0007cdf007fc213ef3ae
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-04-26 09:35:23 -0700

    network

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 import urllib.request
 import sys
+import requests
 from urllib.parse import urlencode
 if __name__ == "__main__":
     val = sys.argv[1]
     em = sys.argv[2]
-    num = urlencode({'email': em})
-    #req =  urllib.request.Request(val, data)
-    data = num.encode('utf-8')
-    print(data)
-    with urllib.request.urlopen(val, data) as response:
-        print(response.read().decode('utf-8'))
+    data = urlencode({"email": em})
+    req =  requests.post(val, data)
+    print(req.txt)
