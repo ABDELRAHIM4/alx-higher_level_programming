8980c64d7efd42e86d26cc4a9d78cbdd7f720ed4
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-04-26 17:26:16 -0700

    network

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL"""
+import urllib.request
+import sys
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as res:
+            print(res.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
