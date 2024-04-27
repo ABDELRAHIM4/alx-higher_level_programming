edbf90f66b2ed59be9d1edd9f0ddecd125d5a628
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-04-26 17:53:15 -0700

    network

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL"""
+import sys
+import requests
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {'email' : email}
+    res = requests.post(url, data=data)
+    print("Your email is: {}".format(email))
