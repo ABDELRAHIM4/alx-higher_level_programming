1c7be2c506a2aeb2c4ceaba73b93307e43a828dc
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-04-27 17:24:43 -0700

    network

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,11 +5,13 @@ import requests
 if __name__ == "__main__":
     repository_name = sys.argv[1]
     owner_name = sys.argv[2]
-    url = f"https://api.github.com/repos/{owner_name}/{repository_name}/commits"
-    response = requests.get(url)
+    ur = f"https://api.github.com/repos/{owner_name}/{repository_name}/commits"
+    response = requests.get(ur)
     if response.status_code == 200:
         reqs = response.json()
         for req in reqs[-10:]:
             sha = req["sha"]
             name = req["author"]["login"]
             print(f"{sha}: {name}")
+    else:
+        print("Error")
