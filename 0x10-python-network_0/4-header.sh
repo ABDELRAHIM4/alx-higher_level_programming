7843554d4b1c044a10aa52d4becdb64d5d334a23
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-04-29 16:07:12 -0700

    network

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL as an argumen
-curl -sH "X-School-User-Id: 98 " $"{1}"
-#curl -v -s -o /dev/null -w "%{http_code}" -H "X-School-User-Id: 98" -X GET "http://0.0.0.0:5000/route_5"; echo
+curl -s -H "X-School-User-Id: 98" "$1"
