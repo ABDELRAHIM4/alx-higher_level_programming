0e78820ed64090cbb688da12b2e7c4554649582f
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-04-30 06:38:55 -0700

    network

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that makes a request to 0.0.0.0:5000/catch_me
-curl -s -X GET http://0.0.0.0:5000/catch_me | grep -o "You got me!"
+curl -s  -X GET 0.0.0.0:5000/catch_me | grep -o "You got me!"
